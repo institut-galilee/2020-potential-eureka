2b04b8b4419eb444d0f16330841542faa02cfcbe
EmilianoBoussac <52208904+EmilianoBoussac@users.noreply.github.com>
2020-02-15 10:34:37 +0100

    Update EspReceiveSerial.ino

--- lab/2/report/2/EspReceiveSerial.ino
+++ lab/2/report/2/EspReceiveSerial.ino
@@ -2,7 +2,7 @@ HardwareSerial Serial1(0);
 void setup() {
   // initialize both serial ports:
   Serial.begin(9600);
-  Serial1.begin(9600);
+  Serial1.begin(9600); // pour recevoir data de rx et tx
 }
 void loop() {
   
