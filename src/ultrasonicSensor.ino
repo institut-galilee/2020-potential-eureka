0a6305dc43bc4dc2735d55e285cf5138ae7f5974
EmilianoBoussac <52208904+EmilianoBoussac@users.noreply.github.com>
2020-04-19 20:19:33 +0200

    Add files via upload

---
+++ src/ultrasonicSensor.ino
@@ -0,0 +1,42 @@
+// defines pins numbers
+const int trigPin = 9;
+const int echoPin = 10;
+// defines variables
+long duration;
+int distance;
+
+#include <LiquidCrystal.h> // includes the LiquidCrystal Library
+LiquidCrystal lcd(1, 2, 4, 5, 6, 7); // Creates an LCD object. Parameters: (rs, enable, d4, d5, d6, d7)
+
+void setup() {
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  Serial.begin(9600); // Starts the serial communication
+}
+void loop() {
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  distance = duration * 0.034 / 2;
+
+  // Prints the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.println(distance);
+
+  //affichage LCD
+
+  lcd.setCursor(0, 0); // Sets the location at which subsequent text written to the LCD will be displayed
+  lcd.print("Distance: "); // Prints string "Distance" on the LCD
+  lcd.print(distance); // Prints the distance value from the sensor
+  lcd.print(" cm");
+  delay(10);
+  lcd.setCursor(0, 1);
+
+}
